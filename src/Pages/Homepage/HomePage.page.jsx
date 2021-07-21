d041ba17c90cdc7830593e2f45cba0353b0eba2c
Nanyam Shukla <nanyam.shukla@olx.com>
2021-07-21 18:09:10 +0530

    Created app using npx cra and created homepage having Directory and MenuItem components

---
+++ src/Pages/Homepage/HomePage.page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.styles.scss';
+
+import Directory from '../../Components/Directory/Directory.component';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
