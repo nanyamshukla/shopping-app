6d8016b51c69a5c97bd1d455759ba5d6b3139527
Nanyam Shukla <nanyam.shukla@olx.com>
2021-08-17 18:40:23 +0530

    Created reducer to keep count of each item added and also handled duplicate entried by adding quantity param

--- src/Components/CollectionItem/CollectionItem.component.js
+++ src/Components/CollectionItem/CollectionItem.component.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './CollectionItem.styles.scss';
+import CustomButton from '../CustomButton/CustomButton.component';
+import { addItem } from '../../Redux/Cart/cart.actions';
 
-const CollectionItem = ({ name, imageUrl, price }) => {
-
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
     return(
         <div className='collection-item'>
             <div 
@@ -16,8 +19,15 @@ const CollectionItem = ({ name, imageUrl, price }) => {
                 <span className='name'> {name} </span>
                 <span className='price'> {price} </span>
             </div>
+            <CustomButton inverted={true} onClick={() => addItem(item)} >
+                ADD TO CART
+            </CustomButton>
         </div>
     )
 }
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
