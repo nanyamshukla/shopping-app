98b989d12e9f859a88fa91521adfadd6c27015e2
Nanyam Shukla <nanyam.shukla@olx.com>
2021-08-28 20:49:47 +0530

    Setup firbase utils to store and fetch shop data to and from firestore. Also created withSpinner HOC. Added redux-thunk as well.

--- src/Pages/Shop/ShopPage.page.jsx
+++ src/Pages/Shop/ShopPage.page.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
+import withSpinner from '../../HOC/withSpinner/withSpinner.hoc';
+
 import CollectionPage from '../Collection/Collection.page';
-
 import CollectionsOverview from '../../Components/CollectionsOverview/CollectionsOverview.component'; 
-const ShopPage = ({ match }) => (
-    <div className="shop-page">
-        <Route exact path={match.path} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-)
 
-export default ShopPage;
+import { fetchCollectionsStartAsync } from '../../Redux/Shop/shop.actions';
+import { selectIsCollectionsFetching } from '../../Redux/Shop/shop.selectors';
+
+const CollectionPageWithSpinner = withSpinner(CollectionPage);
+const CollectionsOverviewWithSpinner = withSpinner(CollectionsOverview);
+
+class ShopPage extends React.Component {
+    componentDidMount() {
+        const { fetchCollectionsStartAsync } = this.props;
+        fetchCollectionsStartAsync();
+    }
+
+    render() {
+        const { match, isCollectionFetching } = this.props;
+        return (
+            <div className="shop-page">
+                <Route 
+                    exact 
+                    path={match.path} 
+                    render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />}
+                />
+                <Route 
+                    path={`${match.path}/:collectionId`} 
+                    render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />}
+                />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    isCollectionFetching: selectIsCollectionsFetching(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default withSpinner(connect(mapStateToProps, mapDispatchToProps)(ShopPage));
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middleWares = [];
+const middleWares = [thunk];
 
 if(process.env.NODE_ENV === 'development') {
     middleWares.push(logger);
@@ -13,5 +14,3 @@ if(process.env.NODE_ENV === 'development') {
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 export const persistor = persistStore(store);
-
-// export default { store, persistor };
